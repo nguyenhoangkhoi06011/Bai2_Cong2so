907b92b26828b3eed128e59fff8897d4b0af7331
nguyenhoangkhoi06011 <nguyenhoangkhoi012021@gmail.com>
2024-11-04 15:41:18 +0700

    bai so3

--- Bai2_Cong2so.cpp
+++ Bai2_Cong2so.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 using namespace std;
 int main() {
-	cout << "CHUONG TRINH TINH TOAN TRONG HAI SO" << endl;
-	int so1, so2; //khai bao bien so1 va so2 kieu so nguyen
-	int tong;
+	cout << "CHUONG TRINH TINH CAC PHEP TINH CO BAN\n";
+	double so1, so2, tong, hieu, tich, thuong;
 	cout << "nhap so1:"; cin >> so1;
 	cout << "Nhap so2:"; cin >> so2;
 	tong = so1 + so2;
-	cout << "tong hai so:" << so1 << "+" << so2 << "=" << tong << endl;
-	cout << "tong hai so:" << so1 << "+" << so2 << "=" << so1 + so2;
+	hieu = so1 - so2;
+	tich = so1 * so2;
+	thuong = so1 / so2;
+	cout << " tong:" << so1 << "+" << so2 << "=" << tong << endl;
+	cout << " hieu:" << so1 << "-" << so2 << "=" << hieu << endl;
+	cout << "tich:" << so1 << "*" << so2 << "=" << tich << endl;
+	cout << "thuong:" << so1 << "/" << so2 << "=" << thuong << endl;
+	
 	return 0;
 }
