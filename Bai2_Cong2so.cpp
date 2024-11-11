fc94443c75a88031d41556fc09ec19a0fbcc991d
nguyenhoangkhoi06011 <nguyenhoangkhoi012021@gmail.com>
2024-11-11 09:38:25 +0700

    tinhpheptinhcoban

--- Bai2_Cong2so.cpp
+++ Bai2_Cong2so.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
+double tinhtong(double a, double b) {
+	return a + b;
+}
+double tinhhieu(double a, double b) {
+	return a - b;
+}
+double tinhtich(double a, double b) {
+	return a * b;
+}
+double tinhthuong(double a, double b) {
+	return round(a / b);
+}
 int main() {
-	cout << "CHUONG TRINH TINH CAC PHEP TINH CO BAN\n";
+cout << "CHUONG TRINH TINH CAC PHEP TINH CO BAN\n";
 	double so1, so2, tong, hieu, tich, thuong;
 	cout << "nhap so1:"; cin >> so1;
 	cout << "Nhap so2:"; cin >> so2;
@@ -9,10 +21,12 @@ int main() {
 	hieu = so1 - so2;
 	tich = so1 * so2;
 	thuong = so1 / so2;
-	cout << " tong:" << so1 << "+" << so2 << "=" << tong << endl;
+	cout << " tong hai so thong thuong:" << so1 << "+" << so2 << "=" << tong << endl;
+	cout << " tong hai so su dung ham:" << so1 << "+" << so2 << "=" << tinhtong(so1,so2) << endl;
 	cout << " hieu:" << so1 << "-" << so2 << "=" << hieu << endl;
 	cout << "tich:" << so1 << "*" << so2 << "=" << tich << endl;
 	cout << "thuong:" << so1 << "/" << so2 << "=" << thuong << endl;
-	
+	system("pause");
 	return 0;
 }
+	
